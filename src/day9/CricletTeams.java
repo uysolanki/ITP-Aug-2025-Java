f22047396cc67cda8eafadee8a59668ca0c51424
uysolanki <uysolanki@gmail.com>
2025-09-11 14:59:51 +0530

    cricket team

--- src/day9/CricletTeams.java
+++ src/day9/CricletTeams.java
@@ -1,19 +1,33 @@
 package day9;
 
+import java.util.Scanner;
+
 public class CricletTeams {
 
 	public static void main(String[] args) {
-		int players[]= {4,5,3,6};
-		int teamSize=3;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of countries");
+		int size=sc.nextInt();
+		
+		int players[]= new int[size];
+		
+		for(int i=0;i<size;i++)
+		{
+			System.out.println("Enter number of players from country " + (i+1));
+			players[i]=sc.nextInt();
+		}
+		
+		System.out.println("Enter Team Size");
+		int teamSize=sc.nextInt();
+
 		
 		int maxNumberOfTeams = findmaxTeams(players,teamSize);
 		System.out.println(maxNumberOfTeams);
-
 	}
 
 	private static int findmaxTeams(int[] players, int teamSize) {
 		int first=1;
-		int last=10;
+		int last=100;
 		int mid;
 		while(first<=last)
 		{
@@ -41,10 +55,10 @@ public class CricletTeams {
 	}
 
 	private static boolean isMidTeamsPossible(int[] players, int mid, int teamSize) {
-		int totalPlayers=mid*teamSize;				//teamSize=20     mid=5
+		int totalPlayers=mid*teamSize;				//teamSize=24    mid=5
 		
 		int sum=0;
-		for(int i=0;i<players.length;i++)		//{4,5,3,6}			sum 0,4,9,12,17
+		for(int i=0;i<players.length;i++)		//{4,5,3,6}			sum 0,4,9,12,18
 		{
 			sum+=Math.min(mid, players[i]);
 		}
